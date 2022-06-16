2198a0fd9ca23f2536c317a67a1db97b58f6679b
st52165 <fanda.silar@seznam.cz>
2022-06-16 05:59:35 +0200

    Wagon Frontend Service and Components Pagination And Order Added.

--- react-bezpecnostzeleznic/src/app/component/wagon/WagonTable.js
+++ react-bezpecnostzeleznic/src/app/component/wagon/WagonTable.js
@@ -3,17 +3,11 @@ import AppNavbar from "../nav/AppNavbar";
 import {Container} from "reactstrap";
 import React, {useEffect, useState} from "react";
 import {Alert, Pagination} from "react-bootstrap";
-import AuthenticationService from "../../service/AuthenticationService";
-import CarrierService from "../../service/CarrierService";
 import WagonService from "../../service/WagonService";
-import filterFactory, {selectFilter, textFilter} from "react-bootstrap-table2-filter";
 
 
 function WagonTable() {
     const [wagons, setWagons] = useState([]);
-    const [wagonTypes, setWagonTypes] = useState([]);
-    const [carriers, setCarriers] = useState([]);
-    const [isAdmin, setAdmin] = useState(false);
     const [pageNumber, setPageNumber] = useState(1);
     const [pageSize, setPageSize] = useState(10);
     const [sortBy, setSortBy] = useState("id");
@@ -25,41 +19,6 @@ function WagonTable() {
         });
     }, [setWagons, pageNumber, pageSize, sortBy, direction]);
 
-    useEffect(() => {
-        const authorities = AuthenticationService.getAuthorities();
-        const admin = authorities.find(it => it.authority === 'ROLE_ADMIN_DS')
-        if (admin != null) {
-            setAdmin(true);
-            CarrierService.getCarriers().then(response => {
-                setCarriers(response.data);
-            });
-        } else {
-            setCarriers([]);
-            setAdmin(false);
-        }
-    }, [setCarriers]);
-
-    useEffect(() => {
-        WagonService.getAllWagonTypes().then((response) => {
-            setWagonTypes(response.data);
-        });
-    }, [setWagonTypes]);
-
-
-    const carrierOptions = carriers.map(carrier => (
-        {
-            value: carrier.name,
-            label: carrier.name
-        }
-    ));
-
-    const wagonTypesOptions = wagonTypes.map(wagonType => (
-        {
-            value: wagonType.name,
-            label: wagonType.name
-        }
-    ));
-
     let onSortFunction = (field, newDirection) => {
         setDirection(newDirection)
         setSortBy(field);
@@ -69,7 +28,6 @@ function WagonTable() {
     const columns = [{
         dataField: 'id',
         text: 'ID',
-        filter: textFilter(),
         sort: true,
         onSort: onSortFunction,
         sortFunc: sortFunction,
@@ -77,18 +35,12 @@ function WagonTable() {
     }, {
         dataField: 'wagonType',
         text: 'Typ vagónu',
-        filter: selectFilter({
-            options: wagonTypesOptions
-        }),
         sort: true,
         onSort: onSortFunction,
         sortFunc: sortFunction
     }, {
         dataField: 'carrierDto.name',
         text: 'Dopravce',
-        filter: isAdmin ? selectFilter({
-            options: carrierOptions
-        }) : null,
         sort: true,
         onSort: (_field, newDirection) => onSortFunction('carrier.name', newDirection),
         sortFunc: sortFunction
@@ -116,8 +68,7 @@ function WagonTable() {
                     <h1>Vagóny</h1>
                 </Alert>
                 <br/>
-                <BootstrapTable keyField='id' data={wagons} columns={columns} filter={filterFactory()}
-                                hover bootstrap4 label/>
+                <BootstrapTable keyField='id' data={wagons} columns={columns} hover bootstrap4 label/>
 
                 <div style={{display: "flex", justifyContent: "flex-start", gap: "22.5em"}}>
                     <select className=" btn dropdown-toggle react-bs-table-sizePerPage-dropdown dropdown"
